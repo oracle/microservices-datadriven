903a5845b94e6686e15620b8522f538673808167
Paul Parkinson <paul.parkinson@gmail.com>
2021-03-23 18:07:48 -0400

    dotnet gitignore

--- grabdish/inventory-dotnet/Controllers/InventoryController.cs
+++ grabdish/inventory-dotnet/Controllers/InventoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Oracle.ManagedDataAccess.Client;
 
 namespace inventory_dotnet.Controllers
 {
@@ -13,7 +14,7 @@ namespace inventory_dotnet.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Sushi", "Pizza", "Burger"
         };
 
         private readonly ILogger<InventoryController> _logger;
@@ -26,6 +27,25 @@ namespace inventory_dotnet.Controllers
         [HttpGet]
         public IEnumerable<Inventory> Get()
         {
+                //https://www.oracle.com/database/technologies/appdev/dotnet/odp.html
+                //dotnet add package Oracle.ManagedDataAccess.Core --version 3.21.1
+                string conString = "User Id=<user>;Password=<password>;Data Source=<data source>;";
+                OracleConnection con = new OracleConnection();
+                con.ConnectionString = conString;
+                con.Open();
+                OracleCommand cmd = con.CreateCommand();
+                cmd.CommandText = "INSERT INTO j_order (po_document) VALUES (:1)";
+                OracleParameter param = new OracleParameter();
+                param.OracleDbType = OracleDbType.Varchar2;
+                param.Value = @"{'id': 66,'name': 'sushi', 'location': 'Philadelphia'}";
+                cmd.Parameters.Add(param);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("JSON inserted.");
+                Console.WriteLine();
+                cmd.CommandText =  "SELECT po_document FROM j_order WHERE JSON_EXISTS (po_document, '$.location')";
+                OracleDataReader rdr = cmd.ExecuteReader();
+                rdr.Read();
+                Console.WriteLine(rdr.GetOracleValue(0));
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new Inventory
             {
