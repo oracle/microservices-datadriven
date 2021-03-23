a9821941fcae973c33bee8c9a693c4dee9757e95
Paul Parkinson <paul.parkinson@gmail.com>
2021-03-23 17:35:34 -0400

    inventory-dotnet init

---
+++ grabdish/inventory-dotnet/Controllers/InventoryController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace inventory_dotnet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InventoryController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<InventoryController> _logger;
+
+        public InventoryController(ILogger<InventoryController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Inventory> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Inventory
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
