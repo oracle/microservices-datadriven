75ed5a161feb808336d52613491af1dd955f0946
RichardExley <Richard.exley@oracle.com>
2021-04-17 06:45:28 -0700

    Update oke-setup.sh
    Hardening

--- grabdish/utils/oke-setup.sh
+++ grabdish/utils/oke-setup.sh
@@ -61,8 +61,12 @@ done
 
 # Create SSL Secret
 while ! state_done SSL_SECRET; do
-  kubectl create secret tls ssl-certificate-secret --key $GRABDISH_HOME/tls/tls.key --cert $GRABDISH_HOME/tls/tls.crt -n msdataworkshop
-  state_set_done SSL_SECRET
+  if kubectl create secret tls ssl-certificate-secret --key $GRABDISH_HOME/tls/tls.key --cert $GRABDISH_HOME/tls/tls.crt -n msdataworkshop; then
+    state_set_done SSL_SECRET
+  else
+    echo "SSL Secret creation failed.  Retrying..."
+    sleep 10
+  fi
 done
 
 
