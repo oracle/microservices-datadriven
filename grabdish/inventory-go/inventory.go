888dbc28aea83c47a2acdcd68226c98513380cf5
Paul Parkinson <paul.parkinson@gmail.com>
2021-04-23 15:47:00 -0400

    inventory-go work

--- grabdish/inventory-go/inventory.go
+++ grabdish/inventory-go/inventory.go
@@ -8,12 +8,17 @@ import (
 )
 
 func main(){
-//         os.Setenv("TNS_ADMIN", "/Users/pparkins/Downloads/Wallet_grabdish4X2");
-        fmt.Println("concat os.Getenv(TNS_ADMIN):" + os.Getenv("TNS_ADMIN"));
-    fmt.Printf("os.Getenv(TNS_ADMIN): %s\n", os.Getenv("TNS_ADMIN"))
-        fmt.Println("About to get connection...")
+    tnsAdmin := os.Getenv("TNS_ADMIN")
+    user := os.Getenv("user")
+    inventoryPDBName := os.Getenv("INVENTORY_PDB_NAME")
+    dbpassword := os.Getenv("dbpassword")
+    fmt.Println("os.Getenv(TNS_ADMIN): %s", tnsAdmin)
+    fmt.Println("os.Getenv(user): %s", user)
+    fmt.Println("os.Getenv(INVENTORY_PDB_NAME): %s", inventoryPDBName)
+    fmt.Println("os.Getenv(dbpassword): %s", dbpassword)
+    fmt.Println("About to get connection..." + "godror", user + "/" + dbpassword + "@" + inventoryPDBName walletLocation=" + tnsAdmin)
 //     db, err := sql.Open("godror", "INVENTORYUSER/Welcome12345@grabdish4x2_tp?TNS_ADMIN=/Users/pparkins/Downloads/Wallet_grabdish4X2")
-    db, err := sql.Open("godror", "INVENTORYUSER/Welcome12345@grabdish4x2_tp")
+    db, err := sql.Open("godror", user + "/" + dbpassword + "@" + inventoryPDBName walletLocation=" + tnsAdmin)
     if err != nil {
         fmt.Println(err)
         return
