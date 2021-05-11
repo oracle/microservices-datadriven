46dfce97131bc74d284b1d5fb946dcea248d7669
Paul Parkinson <paul.parkinson@gmail.com>
2021-05-10 21:34:04 -0400

    verrazzano/multi-cloud work

--- grabdish/inventory-helidon-se/deploy-verrazzano.sh
+++ grabdish/inventory-helidon-se/deploy-verrazzano.sh
@@ -10,23 +10,23 @@ export ORDER_PDB_NAME="$(state_get ORDER_DB_NAME)"
 export OCI_REGION="$(state_get OCI_REGION)"
 export VAULT_SECRET_OCID=""
 
-echo create order-helidon deployment and service...
+echo create inventory-helidon-se deployment and service...
 export CURRENTTIME=$( date '+%F_%H:%M:%S' )
 echo CURRENTTIME is $CURRENTTIME  ...this will be appended to generated deployment yaml
 
-cp order-helidon-comp.yaml order-helidon-comp-$CURRENTTIME.yaml
+cp inventory-helidon-se-comp.yaml inventory-helidon-se-comp-$CURRENTTIME.yaml
 
 #may hit sed incompat issue with mac
-sed -i "s|%DOCKER_REGISTRY%|${DOCKER_REGISTRY}|g" order-helidon-comp-$CURRENTTIME.yaml
-sed -i "s|%ORDER_PDB_NAME%|${ORDER_PDB_NAME}|g" order-helidon-comp-${CURRENTTIME}.yaml
-sed -i "s|%OCI_REGION%|${OCI_REGION}|g" order-helidon-comp-${CURRENTTIME}.yaml
-sed -i "s|%VAULT_SECRET_OCID%|${VAULT_SECRET_OCID}|g" order-helidon-comp-${CURRENTTIME}.yaml
+sed -i "s|%DOCKER_REGISTRY%|${DOCKER_REGISTRY}|g" inventory-helidon-se-comp-$CURRENTTIME.yaml
+sed -i "s|%ORDER_PDB_NAME%|${ORDER_PDB_NAME}|g" inventory-helidon-se-comp-${CURRENTTIME}.yaml
+sed -i "s|%OCI_REGION%|${OCI_REGION}|g" inventory-helidon-se-comp-${CURRENTTIME}.yaml
+sed -i "s|%VAULT_SECRET_OCID%|${VAULT_SECRET_OCID}|g" inventory-helidon-se-comp-${CURRENTTIME}.yaml
 
 if [ -z "$1" ]; then
-    kubectl apply -f $SCRIPT_DIR/order-helidon-comp-$CURRENTTIME.yaml
-#    kubectl apply -f $SCRIPT_DIR/order-helidon-app.yaml
+    kubectl apply -f $SCRIPT_DIR/inventory-helidon-se-comp-$CURRENTTIME.yaml
+#    kubectl apply -f $SCRIPT_DIR/inventory-helidon-se-app.yaml
 else
-    kubectl apply -f <(istioctl kube-inject -f $SCRIPT_DIR/order-helidon-comp-$CURRENTTIME.yaml) -n msdataworkshop
+    kubectl apply -f <(istioctl kube-inject -f $SCRIPT_DIR/inventory-helidon-se-comp-$CURRENTTIME.yaml) -n msdataworkshop
 fi
 
 #kubectl apply -f $SCRIPT_DIR/order-service.yaml -n msdataworkshop
