2bf9032e47d1751964d24b20bc2281d04571eb91
Paul Parkinson <paul.parkinson@gmail.com>
2021-06-16 00:01:34 -0400

    mongo, postgres, kafka crash tests

--- grabdish/utils/crashrecovery-test.sh
+++ grabdish/utils/crashrecovery-test.sh
@@ -7,7 +7,7 @@ set -e
 
 #Install MongoDB, Postgres, and Kafka
 cd $GRABDISH_HOME/mongodb-kafka-postgres;./install-all.sh
-
+echo "TEST_LOG: Crash Recovery waiting for kafka-broker kafka-cat mongodb postgres to start..."
 SERVICES="kafka-broker kafka-cat mongodb postgres"
 
 for s in $SERVICES; do
@@ -18,12 +18,15 @@ for s in $SERVICES; do
   done
 done
 
+echo "TEST_LOG: Crash Recovery kafka-broker kafka-cat mongodb postgres started"
 
 #Undeploy any existing Order, Inventory, and Supplier Services and deploy the MongoDB, Postgres, and Kafka backed Order and Inventory implementations
 for i in order-helidon inventory-helidon inventory-helidon-se inventory-python inventory-nodejs inventory-dotnet inventory-go supplier-helidon-se; do cd $GRABDISH_HOME/$i; ./undeploy.sh; done
 cd $GRABDISH_HOME/order-mongodb-kafka ; ./deploy.sh
 cd $GRABDISH_HOME/inventory-postgres-kafka ; ./deploy.sh
 cd $GRABDISH_HOME
+
+echo "TEST_LOG: Crash Recovery undeployed all and deployed mongodb and postgres services"
 
 SERVICES="order-mongodb-kafka inventory-postgres-kafka"
 for s in $SERVICES; do
@@ -33,6 +36,8 @@ for s in $SERVICES; do
     sleep 5
   done
 done
+
+echo "TEST_LOG: Crash Recovery mongodb and postgres pods running "
 
 #Run tests against order-mongodb-kafka inventory-postgres-kafka implementations
 SERVICES="mongodb-postgres-kafka"
@@ -44,6 +49,8 @@ for s in $SERVICES; do
   ORDER_ID=$(($ORDER_ID + 1000))
   utils/func-test.sh "Crash test (success runs) $s" $ORDER_ID $s
 done
+
+echo "TEST_LOG: Crash Recovery mongodb and postgres tests complete "
 
 #Undeploy MongoDB, Postgres, and Kafka backed Order and Inventory implementations and deploy the Oracle + TEQ/AQ backed Order and Inventory implementations by copying and running the following commands.
 for i in order-mongodb-kafka inventory-postgres-kafka; do cd $GRABDISH_HOME/$i; ./undeploy.sh; done
