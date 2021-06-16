63be61f46cc60005139dba9898081bb581be4412
Paul Parkinson <paul.parkinson@gmail.com>
2021-06-15 23:47:01 -0400

    mongo, postgres, kafka crash tests

--- grabdish/utils/polyglot-test.sh
+++ grabdish/utils/polyglot-test.sh
@@ -6,8 +6,7 @@
 set -e
 
 # Deploy each inventory service and perform functional test
-#SERVICES="inventory-python inventory-nodejs inventory-dotnet inventory-go inventory-helidon-se"
-SERVICES="inventory-nodejs inventory-dotnet inventory-go inventory-helidon-se"
+SERVICES="inventory-python inventory-nodejs inventory-dotnet inventory-go inventory-helidon-se"
 ORDER_ID=66
 
 cd $GRABDISH_HOME/inventory-helidon
