94fc72f5aa9cd16dd31efb31a8c9609b1ae69ca5
Paul Parkinson <paul.parkinson@gmail.com>
2021-06-15 22:19:50 -0400

    mongo, postgres, kafka crash tests

--- grabdish/utils/main-test.sh
+++ grabdish/utils/main-test.sh
@@ -93,14 +93,14 @@ utils/func-test.sh Walkthrough 66
 
 
 # POLYGLOT
-#echo "TEST_LOG: #### Testing Lab3: Polyglot"
-## Deploy each inventory service and perform functional test
-#while ! $(state_get NON_JAVA_BUILDS); do
-#  sleep 10
-#  echo "Waiting for NON_JAVA_BUILDS"
-#done
-#
-#utils/polyglot-test.sh
+echo "TEST_LOG: #### Testing Lab3: Polyglot"
+# Deploy each inventory service and perform functional test
+while ! $(state_get NON_JAVA_BUILDS); do
+  sleep 10
+  echo "Waiting for NON_JAVA_BUILDS"
+done
+
+utils/polyglot-test.sh
 
 
 ## SCALING
