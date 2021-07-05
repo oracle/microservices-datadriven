36c5dadddaca1673cf051e863f92c57e2edd5234
Paul Parkinson <paul.parkinson@gmail.com>
2021-07-04 23:05:54 -0400

    observability work

--- grabdish/observabilty/prometheus-grafana/install.sh
+++ grabdish/observabilty/prometheus-grafana/install.sh
@@ -10,21 +10,14 @@ kubectl get prometheus -n msdataworkshop
 kubectl patch svc stable-kube-prometheus-sta-prometheus -p '{"spec": {"type": "LoadBalancer"}}' -n msdataworkshop
 kubectl patch svc stable-grafana -p '{"spec": {"type": "LoadBalancer"}}' -n msdataworkshop
 kubectl apply -f prom_msdataworkshop_servicemonitor.yaml -n msdataworkshop
-# admin/prom-operator
-#MicroProfile https://grafana.com/grafana/dashboards/12853
-#Fault Tolerance https://grafana.com/grafana/dashboards/8022
-kubectl create configmap db-teq-exporter-config --from-file=CUSTOM_METRICS=./db-teq-exporter-metrics.toml -n msdataworkshop
-kubectl create configmap db-teq-exporter-config ./db-teq-exporter-metrics.toml -n msdataworkshop
+kubectl create configmap db-teq-exporter-config --from-file=./db-teq-exporter-metrics.toml -n msdataworkshop
+kubectl describe configmap db-teq-exporter-config -n msdataworkshop
 kubectl get configmap db-teq-exporter-config  -n msdataworkshop -o yaml
+#kubectl delete configmap db-teq-exporter-config  -n msdataworkshop -o yaml
 kubectl apply -f db-teq-exporter-deployment.yaml -n msdataworkshop
 kubectl apply -f db-teq-exporter-service.yaml -n msdataworkshop
 
 kubectl apply -f grafana.yaml -n msdataworkshop
-
-#docker run -d --name oracledb_exporter --link=oracle -p 9161:9161
-#--env-file /Users/pparkins/go/src/orahub/ora-jdbc-dev/aqkafka-java-client/scripts/monitoring/.envtest
-#-v /Users/pparkins/go/src/orahub/ora-jdbc-dev/aqkafka-java-client/scripts/monitoring/exporter/default-metrics.toml:/default-metrics.toml
-#iamseth/oracledb_exporter
-#
-#DATA_SOURCE_NAME=system/oracle@oracle/xe
-#CUSTOM_METRICS=/default-metrics.toml
+# admin/prom-operator
+#MicroProfile https://grafana.com/grafana/dashboards/12853
+#Fault Tolerance https://grafana.com/grafana/dashboards/8022
