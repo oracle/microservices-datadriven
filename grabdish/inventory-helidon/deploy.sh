98306dd0aedc53ae98a568bb6fb9a04bf035d07f
Paul Parkinson <paul.parkinson@gmail.com>
2021-09-01 14:37:44 -0400

    enable teq

--- grabdish/inventory-helidon/deploy.sh
+++ grabdish/inventory-helidon/deploy.sh
@@ -16,7 +16,7 @@ fi
 
 if [ -z "$INVENTORY_PDB_NAME" ]; then
     echo "INVENTORY_PDB_NAME not set. Will get it with state_get"
-  export INVENTORY_PDB_NAME=$(state_get inventory_DB_NAME)
+  export INVENTORY_PDB_NAME=$(state_get INVENTORY_PDB_NAME)
 fi
 
 if [ -z "$INVENTORY_PDB_NAME" ]; then
