3cc152c9292b63fb57629b8e4c6b93fd4c918348
RichardExley <Richard.exley@oracle.com>
2021-11-29 15:37:02 -0800

    Reorganized non-java-builds

--- grabdish/utils/non-java-builds.sh
+++ grabdish/utils/non-java-builds.sh
@@ -5,8 +5,12 @@
 # Fail on error
 set -e
 
-BUILDS="inventory-python inventory-nodejs inventory-dotnet inventory-go inventory-helidon-se order-mongodb-kafka inventory-postgres-kafka inventory-springboot inventory-micronaut inventory-quarkus foodwinepairing-python"
+P2_BUILDS="foodwinepairing-python"
+P3_NON_JAVA_BUILDS="inventory-python inventory-nodejs inventory-dotnet inventory-go"
+# Run serially (Java)
+P3_JAVA_BUILDS="inventory-helidon-se order-mongodb-kafka inventory-postgres-kafka inventory-springboot inventory-micronaut inventory-quarkus"
 # we provision a repos for db-log-exporter but it's in nested observability/db-log-exporter dir so not reusing BUILDS list to build (see DB_LOG_EXPORTER_BUILD below)
+
 REPOS="inventory-python inventory-nodejs inventory-dotnet inventory-go inventory-helidon-se order-mongodb-kafka inventory-postgres-kafka inventory-springboot inventory-micronaut inventory-micronaut-native-image inventory-quarkus db-log-exporter foodwinepairing-python"
 
 # Provision Repos
@@ -32,19 +36,44 @@ while ! state_done JAVA_BUILDS; do
 done
 
 
-# Build  the images
-while ! state_done NON_JAVA_BUILDS; do
-  for b in $BUILDS; do
+# Build P2_BUILDS images
+while ! state_done P2_BUILDS; do
+  for b in $P2_BUILDS; do
     cd $GRABDISH_HOME/$b
+    echo "### building $b"
     time ./build.sh &>> $GRABDISH_LOG/build-$b.log &
   done
   wait
-  state_set_done NON_JAVA_BUILDS
+  state_set_done P2_BUILDS
 done
+
+# Build P3_BUILDS images
+while ! state_done P3_BUILDS; do
+  for b in $P3_NON_JAVA_BUILDS; do
+    cd $GRABDISH_HOME/$b
+    echo "### building $b"
+    time ./build.sh &>> $GRABDISH_LOG/build-$b.log &
+  done
+
+  # Run serially because maven is unstable in parallel
+  for b in $P3_JAVA_BUILDS; do
+    cd $GRABDISH_HOME/$b
+    echo "### building $b"
+    time ./build.sh &>> $GRABDISH_LOG/build-$b.log
+  done
+  wait
+  state_set_done P3_BUILDS
+done
+
+if ! state_done NON_JAVA_BUILDS; then
+  state_set_done NON_JAVA_BUILDS
+fi
+
 
 # Build the db-log-exporter image
 while ! state_done DB_LOG_EXPORTER_BUILD; do
   cd $GRABDISH_HOME/observability/db-log-exporter
+  echo "### building db-log-exporter"
   time ./build.sh &>> $GRABDISH_LOG/build-db-log-exporter.log &
   state_set_done DB_LOG_EXPORTER_BUILD
 done
