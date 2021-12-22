69cf7ccf4ea6fbf125e26529dd522e68b29c90cd
RichardExley <Richard.exley@oracle.com>
2021-12-22 15:48:47 -0800

    Update destroy.sh

--- workshops/dcms-oci/config/threads/db/destroy.sh
+++ workshops/dcms-oci/config/threads/db/destroy.sh
@@ -30,11 +30,11 @@ done
 DBS="db1 db2"
 for db in $DBS; do
   db_upper=`echo $db | tr '[:lower:]' '[:upper:]'`
-  if ! state_set ${db_upper}_OCID; then
+  if ! state_done ${db_upper}_OCID; then
     # Already destroyed
     continue
   fi
-  if test $(state_get RUN_TYPE) == "LL" || test -z "$(state_get ${db_upper}_OCID)"; then
+  if test "$(state_get RUN_TYPE)" == "LL" || test -z "$(state_get ${db_upper}_OCID)"; then
     # Nothing to destroy
     :
   else
