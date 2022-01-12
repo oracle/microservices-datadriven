ab560b1992d7b1a56649b97a1e3c84d5ebafa266
Paul Parkinson <paul.parkinson@gmail.com>
2022-01-11 20:17:00 -0500

    saga ws

--- travelbooking/getWalletForPDBs.sh
+++ travelbooking/getWalletForPDBs.sh
@@ -2,6 +2,7 @@
 # Copyright (c) 2021 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
+cd ~/microservices-datadriven/travelbooking
 mkdir wallet
 cd wallet
 oci db autonomous-database generate-wallet --autonomous-database-id "$1" --file 'wallet.zip' --password 'Welcome1' --generate-type 'ALL'
