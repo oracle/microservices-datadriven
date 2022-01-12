c0fcb67d69697eb504c74471b0ff33ec9292255c
Paul Parkinson <paul.parkinson@gmail.com>
2022-01-11 20:50:06 -0500

    saga ws

--- travelbooking/getWalletForPDBs.sh
+++ travelbooking/getWalletForPDBs.sh
@@ -2,6 +2,13 @@
 # Copyright (c) 2022 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
+
+if [ -z "$1" ]; then
+    echo "Error: ocid of either osagadb1 or osagadb2 21c database must be provided as argument."
+    echo "For example: ./getWalletForPDBs.sh ocid1.autonomousdatabase.oc1.phx.anyxxxxxxxxxxxx"
+    exit 1
+fi
+
 cd ~/microservices-datadriven/travelbooking
 mkdir wallet
 cd wallet
