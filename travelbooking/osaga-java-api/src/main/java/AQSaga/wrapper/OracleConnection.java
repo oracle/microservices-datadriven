9f771850a448b9fc8342b87836adfd2c55b5a871
Paul Parkinson <paul.parkinson@gmail.com>
2022-01-11 20:58:50 -0500

    saga ws

--- travelbooking/osaga-java-api/src/main/java/AQSaga/wrapper/OracleConnection.java
+++ travelbooking/osaga-java-api/src/main/java/AQSaga/wrapper/OracleConnection.java
@@ -1,13 +1,5 @@
 package AQSaga.wrapper;
 
-import AQSaga.Constants;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.nio.charset.StandardCharsets;
 import java.sql.*;
 import java.util.Arrays;
