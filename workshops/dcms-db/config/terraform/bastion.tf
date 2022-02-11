f0a3a786de9af9fa456b7de0599ebcb78c05f03f
RichardExley <Richard.exley@oracle.com>
2022-02-11 10:19:02 -0800

    test

--- workshops/dcms-db/config/terraform/bastion.tf
+++ workshops/dcms-db/config/terraform/bastion.tf
@@ -1,4 +1,4 @@
-# Copyright © 2020, Oracle and/or its affiliates. 
+# Copyright © 2020, Oracle and/or its affiliates.
 # All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 resource "oci_bastion_bastion" "bastion_service" {
@@ -6,7 +6,7 @@ resource "oci_bastion_bastion" "bastion_service" {
   bastion_type                 = "STANDARD"
   target_subnet_id             = oci_core_subnet.subnet_public.id
   client_cidr_block_allow_list = ["0.0.0.0/0"]
-  name                         = format("%sBastionService", var.proj_abrv)
+  name                         = "DCMSDBBastionService"
   max_session_ttl_in_seconds   = 10800
 }
 
