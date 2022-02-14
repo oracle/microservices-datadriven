ea238b092cbe14b2ffc4d9ada96a1637f0c6ec01
Paulo Simoes <paulo.simoes@oracle.com>
2022-02-14 13:08:42 -0300

    echo off user creation command

--- workshops/eventmesh-teq-kafka/cloud-setup/database/oracle_db_user_create.sql
+++ workshops/eventmesh-teq-kafka/cloud-setup/database/oracle_db_user_create.sql
@@ -1,6 +1,6 @@
 -- Copyright (c) 2021 Oracle and/or its affiliates.
 -- Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-set echo on
+set echo off
 
 DECLARE
   my_user  varchar2(30) := '&1' ;
@@ -8,7 +8,6 @@ DECLARE
 
 BEGIN
     --- USER SQL
-    CREATE USER username IDENTIFIED BY password  ;
     EXECUTE IMMEDIATE 'CREATE USER '||my_user||' IDENTIFIED BY '||my_password;
 
     --- GRANT User permissions.
