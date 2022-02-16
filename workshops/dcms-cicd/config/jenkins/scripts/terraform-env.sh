38652ea178bffd81a2eae22381d6c0dd64a547d1
Norman Aberin <aberinnj@gmail.com>
2022-02-16 17:20:02 -0600

    Fixed terraform-env

--- workshops/dcms-cicd/config/jenkins/scripts/terraform-env.sh
+++ workshops/dcms-cicd/config/jenkins/scripts/terraform-env.sh
@@ -1,7 +1,7 @@
 # Terraform variables
 export TF_VAR_region="$(state_get OCI_REGION)"
 export TF_VAR_compartment_ocid="$(state_get COMPARTMENT_OCID)"
-export TF_VAR_jenkins_password="${state_get JENKINS_PASSWORD_SECRET}"
+export TF_VAR_jenkins_password="${get_secret JENKINS_PASSWORD}"
 
 # Setup logging just in case something fails
 export TF_LOG=TRACE
