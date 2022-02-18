f4a10c34e81170797fb3ac55afcdedcb5bd89f45
RichardExley <Richard.exley@oracle.com>
2022-02-18 07:17:17 -0800

    test

--- workshops/dcms-db/config/terraform/output.tf
+++ workshops/dcms-db/config/terraform/output.tf
@@ -1,4 +1,4 @@
 output "lb_address" {
-  value       = oci_core_instance.instance.public_ip
-  description = "The Pubic facing IP Address assigned to the instance"
+  value       = oci_load_balancer.lb.ip_addresses
+  description = "The Public facing IP Address assigned to the instance"
 }
