cc7ce0b2f27a1232f1b0f1fdf752c13ffa8bf905
Paulo Simoes <paulo.simoes@oracle.com>
2022-02-25 10:32:35 -0300

    Changes to be committed: 	modified:   okafka-config/pom.xml 	modified:   okafka-config/src/main/java/com/oracle/developers/oracleteq/okafka/config/consumer/OKafkaConsumerConfig.java 	modified:   okafka-config/src/main/java/com/oracle/developers/oracleteq/okafka/config/consumer/OKafkaConsumerConfigData.java 	modified:   okafka-config/src/main/java/com/oracle/developers/oracleteq/okafka/config/data/LabEventData.java 	modified:   okafka-config/src/main/java/com/oracle/developers/oracleteq/okafka/config/data/LabResponseData.java 	modified:   okafka-config/src/main/java/com/oracle/developers/oracleteq/okafka/config/data/OKafkaConfigData.java 	modified:   okafka-config/src/main/java/com/oracle/developers/oracleteq/okafka/config/data/OKafkaTopicConfigData.java 	modified:   okafka-config/src/main/java/com/oracle/developers/oracleteq/okafka/config/producer/OKafkaProducerConfig.java 	modified:   okafka-config/src/main/java/com/oracle/developers/oracleteq/okafka/config/producer/OKafkaProducerConfigData.java 	modified:   okafka-consumer/pom.xml 	modified:   okafka-consumer/src/main/java/com/oracle/developers/oracleteq/okafka/OKafkaConsumerApplication.java 	modified:   okafka-consumer/src/main/resources/application.yaml 	deleted:    okafka-consumer/src/main/resources/application.yaml_local 	modified:   okafka-producer/src/main/java/com/oracle/developers/oracleteq/okafka/OKafkaProducerApplication.java 	modified:   okafka-producer/src/main/java/com/oracle/developers/oracleteq/okafka/service/OKafkaEventProducer.java 	modified:   okafka-producer/src/main/java/com/oracle/developers/oracleteq/okafka/service/OKafkaProducerService.java 	modified:   okafka-producer/src/main/java/com/oracle/developers/oracleteq/okafka/service/OKafkaProducerServiceController.java 	modified:   okafka-producer/src/main/resources/application.yaml 	deleted:    okafka-producer/src/main/resources/application.yaml_local 	modified:   okafka-producer/src/test/java/com/oracle/developers/oracleteq/okafka/OkafkaApplicationTests.java
     Changes not staged for commit:
    	modified:   ../springboot-kafka/kafka-config/src/main/java/com/oracle/developers/eventmesh/kafka/config/consumer/KafkaConsumerConfig.java
    	modified:   ../springboot-kafka/kafka-config/src/main/java/com/oracle/developers/eventmesh/kafka/config/data/KafkaTopicConfigData.java
    	modified:   ../springboot-kafka/kafka-config/src/main/java/com/oracle/developers/eventmesh/kafka/config/data/LabEventData.java
    	modified:   ../springboot-kafka/kafka-config/src/main/java/com/oracle/developers/eventmesh/kafka/config/data/LabResponseData.java
    	modified:   ../springboot-kafka/kafka-config/src/main/java/com/oracle/developers/eventmesh/kafka/config/producer/KafkaProducerConfig.java

--- workshops/microservices-oracleteq-kafka/springboot-oracleteq/okafka-config/src/main/java/com/oracle/developers/oracleteq/okafka/config/consumer/OKafkaConsumerConfigData.java
+++ workshops/microservices-oracleteq-kafka/springboot-oracleteq/okafka-config/src/main/java/com/oracle/developers/oracleteq/okafka/config/consumer/OKafkaConsumerConfigData.java
@@ -1,3 +1,8 @@
+/*
+ **
+ ** Copyright (c) 2021 Oracle and/or its affiliates.
+ ** Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+ */
 package com.oracle.developers.oracleteq.okafka.config.consumer;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
--- workshops/microservices-oracleteq-kafka/springboot-oracleteq/okafka-producer/src/test/java/com/oracle/developers/oracleteq/okafka/OkafkaApplicationTests.java
+++ workshops/microservices-oracleteq-kafka/springboot-oracleteq/okafka-producer/src/test/java/com/oracle/developers/oracleteq/okafka/OkafkaApplicationTests.java
@@ -1,4 +1,9 @@
-package com.oracle.developers.oracleteq.okafka;
+/*
+ **
+ ** Copyright (c) 2021 Oracle and/or its affiliates.
+ ** Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+ */p
+ackage com.oracle.developers.oracleteq.okafka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
