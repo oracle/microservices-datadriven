998bddf64a8c4ac8e46409a107fcbb9473752144
Paulo Simoes <paulo.simoes@oracle.com>
2022-03-18 03:04:57 -0300

    Fix Issue 353::Item 1 - missing okafka library (jar)

--- workshops/oracleteq-kafka/cloud-setup/okafka/okafka-maven-install.sh
+++ workshops/oracleteq-kafka/cloud-setup/okafka/okafka-maven-install.sh
@@ -3,12 +3,16 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 # Fail on error
-set -e
+set -eu
 
 # PATH to OKafka Library
-OKAFKA_LIB="$LAB_HOME"/cloud-setup/okafka/okafka-0.8.jar
+OKAFKA_LIB="$LAB_HOME"/cloud-setup/okafka/okafka-0.8.lib
+OKAFKA_JAR="$LAB_HOME"/cloud-setup/okafka/okafka-0.8.jar
+
+# rename Lib to Jar
+mv "$OKAFKA_LIB" "$OKAFKA_JAR"
 
 # Install okafka library into Maven local repository.
-mvn install:install-file -Dfile="$OKAFKA_LIB" -DgroupId=org.oracle.okafka -DartifactId=okafka -Dversion=0.8 -Dpackaging=jar
+mvn install:install-file -Dfile="$OKAFKA_JAR" -DgroupId=org.oracle.okafka -DartifactId=okafka -Dversion=0.8 -Dpackaging=jar
 
 state_set_done OKAFKA_INSTALLED
