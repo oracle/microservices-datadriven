26fe1657e54925ed7d345fe7342d3fde1287d9cf
Mark Nelson <mark.x.nelson@oracle.com>
2022-04-19 14:17:12 -0400

    Initial DRA workshop materials (#375)
    * initial load of data for DRA workshop
    
    Signed-off-by: Mark Nelson <mark.x.nelson@oracle.com>

---
+++ workshops/microservices-data-refactoring/env.sh
@@ -0,0 +1,8 @@
+export COMPARTMENT="dra"
+export DB_NAME="dradb"
+export DB_ADMIN_ASER="admin"
+export DB_NORMAL_USER="tkdradata"
+export DRA_HOME=${HOME}/microservices-data-refactoring
+export TNS_ADMIN=$DRA_HOME/wallet
+export DB_ALIAS="dradb_tp"
+export SQLCL=/opt/oracle/sqlcl/lib
