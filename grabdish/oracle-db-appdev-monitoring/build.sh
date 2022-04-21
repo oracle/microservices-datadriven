1b097d631ad1d6418d2a68b6a31f77af5d52c708
Paul Parkinson <paul.parkinson@gmail.com>
2022-04-20 22:07:15 -0400

    add exporter build

--- grabdish/oracle-db-appdev-monitoring/build.sh
+++ grabdish/oracle-db-appdev-monitoring/build.sh
@@ -2,4 +2,5 @@
 ## Copyright (c) 2021 Oracle and/or its affiliates.
 ## Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
-echo will build here
+cd $GRABDISH_HOME/../oracle-db-appdev-monitoring
+./build.sh
