0273e0e832b34cfb4c61afbcab0aa5bc97654531
naberin <aberinnj@gmail.com>
2022-05-20 14:01:54 -0500

    Added initial Files for auth-service

---
+++ cloudbank/cloudbank-frontend-service/src/test/java/com/cloudbank/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudbank.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
