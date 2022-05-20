79eb171c1b16ebac4a3d69947d1bca228a95c861
naberin <aberinnj@gmail.com>
2022-05-20 14:35:16 -0500

    Added fixes for ViewControllers and removed unused react imports

--- cloudbank/cloudbank-frontend-service/src/main/react-app/src/components/Transfer/index.js
+++ cloudbank/cloudbank-frontend-service/src/main/react-app/src/components/Transfer/index.js
@@ -1,7 +1,6 @@
 import TransferNav from "./nav";
 import History from "./history";
 import Transfer from "./transfer";
-import Login from "../Login";
 
 import {Route, Routes} from "react-router-dom";
 import React, {useState} from "react";
