a9dcff729174e33c36343ac1bc633ee23fcbeffe
Paul Parkinson <paul.parkinson@gmail.com>
2022-06-14 15:36:04 -0400

    reconcile test changes

--- grabdish/tests/testAll.sh
+++ grabdish/tests/testAll.sh
@@ -2,12 +2,9 @@
 # Copyright (c) 2022 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-<<<<<<< HEAD:grabdish/tests/testAll.sh
 ./build.sh
 ./testHelidonAndTransactional.sh
 ./testPolyglot.sh
-=======
 REQ_UTILS="touch rm mkdir"
 REQ_INPUT_PARAMS=""
 REQ_OUTPUT_PARAMS="LB_ADDRESS ORDS_ADDRESS TNS_ADMIN"
->>>>>>> upstream/main:workshops/dcms-db/config/requirements.env
