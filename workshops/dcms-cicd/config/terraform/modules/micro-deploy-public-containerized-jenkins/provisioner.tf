cd12a33dca4cb597077559ccfed21010e1b2092d
irina granat <irina.granat@oracle.com>
2022-06-14 15:35:18 -0700

    Create provisioner.tf

---
+++ workshops/dcms-cicd/config/terraform/modules/micro-deploy-public-containerized-jenkins/provisioner.tf
@@ -0,0 +1,13 @@
+resource "null_resource" "upload_wallet" {
+  provisioner "file" {
+    connection {
+      type                = "ssh"
+      agent               = false
+      user                = var.compute_user
+      host                = oci_core_public_ip.jenkins_public_ip.ip_address
+      private_key         = tls_private_key.tls_key_pair.private_key_pem
+    }
+    source      = local_file.database_wallet_file.filename
+    destination = format("/tmp/%sDB_wallet.zip", upper(var.proj_abrv))
+  }
+}
