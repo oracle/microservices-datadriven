cdaa7a7233dcfefb2e380941a281774f30462f3c
Paul Parkinson <paul.parkinson@gmail.com>
2022-06-30 21:20:27 -0400

    observability work

--- grabdish/frontend-helidon/deploy-noLB.sh
+++ grabdish/frontend-helidon/deploy-noLB.sh
@@ -3,4 +3,4 @@
 ## Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
 kubectl delete service ingress-nginx-controller -n msdataworkshop
-k8s-deploy 'frontend-helidon-deployment.yaml frontend-service-nodeport.yaml'
+k8s-deploy 'frontend-helidon-deployment.yaml frontendnp-service-nodeport.yaml'
