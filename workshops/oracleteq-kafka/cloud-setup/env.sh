0201d5e51769f2623983b2b3380dca7646fa050c
Andy Tael <andy.tael@yahoo.com>
2022-09-10 21:41:48 -0500

    Update env.sh

--- workshops/oracleteq-kafka/cloud-setup/env.sh
+++ workshops/oracleteq-kafka/cloud-setup/env.sh
@@ -83,15 +83,15 @@ while ! state_done RUN_NAME; do
     exit
   fi
   # Get the directory name e.g. teqodb -- Hardcoded in lab needs fix
-  DIRNAME=$(basename "$PWD")
+  DN=$(basename "$PWD")
   # Genrate unique'ish variable for Comp Name, DB Name etc
-  DN=teq`awk 'BEGIN { srand(); print int(1 + rand() * 100000001)}'`
+  COMP_NAME=teq`awk 'BEGIN { srand(); print int(1 + rand() * 100000001)}'`
   # Validate run name.  Must be between 1 and 13 characters, only letters or numbers, starting with letter
   if [[ "$DN" =~ ^[a-zA-Z][a-zA-Z0-9]{0,12}$ ]]; then
     # shellcheck disable=SC2046
     state_set RUN_NAME $(echo "$DN" | awk '{print tolower($0)}')
     state_set LAB_DB_NAME "$(state_get RUN_NAME)"
-    state_set DIRNAME $(echo "$DIRNAME" | awk '{print tolower($0)}')
+    state_set COMP_NAME $(echo "$COMP_NAME" | awk '{print tolower($0)}')
   else
     echo "Error: Invalid directory name $RN.  The directory name must be between 1 and 13 characters,"
     echo "containing only letters or numbers, starting with a letter.  Please restart the workshop with a valid directory name."
