ea846d74d8d0424a66b07596f2eba9e6bb7a6b27
John Lathouwers <john.lathouwers@oracle.com>
2022-10-03 16:16:01 +0100

    reshuffle

--- workshops/dcms-cicd/config/terraform/modules/micro-deploy-public-containerized-jenkins/compute.tf
+++ workshops/dcms-cicd/config/terraform/modules/micro-deploy-public-containerized-jenkins/compute.tf
@@ -25,6 +25,6 @@ resource "oci_core_instance" "jenkins_vm" {
 
   metadata = {
     ssh_authorized_keys = var.generate_public_ssh_key ? tls_private_key.tls_key_pair.public_key_openssh : join("\n", [var.public_ssh_key, tls_private_key.tls_key_pair.public_key_openssh])
-    user_data           = base64encode(templatefile("${path.module}/scripts/cloud-init.yaml", ))
+    user_data           = base64encode("${path.module}/scripts/cloud-init.yaml")
   }
 }
