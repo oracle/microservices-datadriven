6f428cac491bd0b0c47efe22d0c91931b5b9c181
John Lathouwers <john.lathouwers@oracle.com>
2022-10-04 10:59:13 +0100

    Clean up provision

--- workshops/dcms-cicd/config/terraform/modules/micro-deploy-public-containerized-jenkins/provision.tf
+++ workshops/dcms-cicd/config/terraform/modules/micro-deploy-public-containerized-jenkins/provision.tf
@@ -1,23 +1,3 @@
-resource "null_resource" "jenkins_provisioner" {
-  depends_on = [oci_core_instance.jenkins_vm, oci_core_public_ip.jenkins_public_ip]
-
-  provisioner "remote-exec" {
-    connection {
-      type        = "ssh"
-      host        = oci_core_public_ip.jenkins_public_ip.ip_address
-      agent       = false
-      timeout     = "5m"
-      user        = "opc"
-      private_key = tls_private_key.tls_key_pair.private_key_pem
-
-    }
-    inline = [
-      "while [ ! -f /tmp/cloud-init-complete ]; do sleep 1; done",
-      "docker-compose -f /home/opc/jenkins.yaml up -d"
-    ]
-  }
-}
-
 resource "null_resource" "upload_wallet" {
   provisioner "file" {
     connection {
