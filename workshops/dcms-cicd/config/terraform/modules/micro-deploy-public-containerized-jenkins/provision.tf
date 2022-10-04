8c57bcc778ed9fab7fc187d874a3bc60594fd91f
John Lathouwers <john.lathouwers@oracle.com>
2022-10-04 10:56:39 +0100

    Move all provisioners into cloud-init

--- workshops/dcms-cicd/config/terraform/modules/micro-deploy-public-containerized-jenkins/provision.tf
+++ workshops/dcms-cicd/config/terraform/modules/micro-deploy-public-containerized-jenkins/provision.tf
@@ -1,60 +1,5 @@
-data "template_file" "jenkins_docker_compose" {
-  template = file("${path.module}/scripts/jenkins.yaml")
-
-  vars = {
-    public_key_openssh = tls_private_key.tls_key_pair.public_key_openssh,
-    jenkins_user       = var.jenkins_user,
-    jenkins_password   = var.jenkins_password
-  }
-}
-
 resource "null_resource" "jenkins_provisioner" {
   depends_on = [oci_core_instance.jenkins_vm, oci_core_public_ip.jenkins_public_ip]
-
-  provisioner "file" {
-    content     = data.template_file.jenkins_docker_compose.rendered
-    destination = "/home/opc/jenkins.yaml"
-
-    connection {
-      type        = "ssh"
-      host        = oci_core_public_ip.jenkins_public_ip.ip_address
-      agent       = false
-      timeout     = "5m"
-      user        = "opc"
-      private_key = tls_private_key.tls_key_pair.private_key_pem
-
-    }
-  }
-
-  provisioner "file" {
-    content     = file("${path.module}/scripts/Dockerfile")
-    destination = "/home/opc/Dockerfile"
-
-    connection {
-      type        = "ssh"
-      host        = oci_core_public_ip.jenkins_public_ip.ip_address
-      agent       = false
-      timeout     = "5m"
-      user        = "opc"
-      private_key = tls_private_key.tls_key_pair.private_key_pem
-
-    }
-  }
-
-  provisioner "file" {
-    content     = file("${path.module}/scripts/casc.yaml")
-    destination = "/home/opc/casc.yaml"
-
-    connection {
-      type        = "ssh"
-      host        = oci_core_public_ip.jenkins_public_ip.ip_address
-      agent       = false
-      timeout     = "5m"
-      user        = "opc"
-      private_key = tls_private_key.tls_key_pair.private_key_pem
-
-    }
-  }
 
   provisioner "remote-exec" {
     connection {
@@ -76,13 +21,13 @@ resource "null_resource" "jenkins_provisioner" {
 resource "null_resource" "upload_wallet" {
   provisioner "file" {
     connection {
-      type                = "ssh"
-      agent               = false
-      user                = opc
-      host                = oci_core_public_ip.jenkins_public_ip.ip_address
-      private_key         = tls_private_key.tls_key_pair.private_key_pem
+      type        = "ssh"
+      agent       = false
+      user        = opc
+      host        = oci_core_public_ip.jenkins_public_ip.ip_address
+      private_key = tls_private_key.tls_key_pair.private_key_pem
     }
-    source      = local_file.database_wallet_file.filename
+    source = local_file.database_wallet_file.filename
     # Place in an existing directory; cloud-init will move it
     destination = "/home/opc/wallet/adb_wallet.zip"
   }
