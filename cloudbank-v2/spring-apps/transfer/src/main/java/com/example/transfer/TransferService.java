3d8be714aca1eacf8fc0af212b4e7f8dd54c6d5e
Mark Nelson <mark.x.nelson@oracle.com>
2023-03-08 15:28:07 -0500

    update transfer service
    Signed-off-by: Mark Nelson <mark.x.nelson@oracle.com>

--- cloudbank-v2/spring-apps/transfer/src/main/java/com/example/transfer/TransferService.java
+++ cloudbank-v2/spring-apps/transfer/src/main/java/com/example/transfer/TransferService.java
@@ -45,13 +45,9 @@ public class TransferService {
 
     @PostConstruct
     private void initController() {
-        try { // todo get from config/env
-            withdrawUri = new URI("http://account.application:8080/withdraw/withdraw");
-            depositUri = new URI("http://account.application:8080/deposit/deposit");
-            transferCancelUri = new URI("http://transfer.application:8080/cancel");
-            transferConfirmUri = new URI("http://transfer.application:8080/confirm");
-            transferProcessCancelUri = new URI("http://transfer.application:8080/processcancel");
-            transferProcessConfirmUri = new URI("http://transfer.application:8080/processconfirm");
+        try { //todo get from Environment instead...
+            withdrawUri = new URI(System.getenv("withdraw.account.service.url"));
+            depositUri = new URI(System.getenv("deposit.account.service.url"));
         } catch (URISyntaxException ex) {
             throw new IllegalStateException("Failed to initialize " + TransferService.class.getName(), ex);
         }
@@ -60,109 +56,40 @@ public class TransferService {
     @POST
     @Path("/transfer")
     @Produces(MediaType.APPLICATION_JSON)
-    @LRA(value = LRA.Type.REQUIRES_NEW, end = false)
-    public Response transfer(@QueryParam("fromAccount") long fromAccount,
-            @QueryParam("toAccount") long toAccount,
-            @QueryParam("amount") long amount,
-            @Context UriInfo uriInfo,
-            @HeaderParam(LRA_HTTP_CONTEXT_HEADER) String lraId,
-            @Context ContainerRequestContext containerRequestContext) {
+    @LRA(value = LRA.Type.REQUIRES_NEW)
+    public Response bookTrip(@QueryParam("fromAccount") String fromAccount,
+                             @QueryParam("toAccount") String toAccount,
+                             @QueryParam("amount") long amount,
+                             @Context UriInfo uriInfo,
+                             @HeaderParam(LRA_HTTP_CONTEXT_HEADER) String lraId,
+                             @Context ContainerRequestContext containerRequestContext)
+    {
         if (lraId == null) {
             return Response.serverError().entity("Failed to create LRA").build();
         }
-        log.info("Started new LRA/transfer Id: " + lraId);
-
-        boolean isCompensate = false;
-        String returnString = "";
-
-        // perform the withdrawal
-        returnString += withdraw(fromAccount, amount);
-        log.info(returnString);
-        if (returnString.contains("succeeded")) {
-            // if it worked, perform the deposit
-            returnString += " " + deposit(toAccount, amount);
-            log.info(returnString);
-            if (returnString.contains("failed"))
-                isCompensate = true; // deposit failed
-        } else
-            isCompensate = true; // withdraw failed
-        log.info("LRA/transfer action will be " + (isCompensate ? "cancel" : "confirm"));
-
-        // call complete or cancel based on outcome of previous actions
-        WebTarget webTarget = ClientBuilder.newClient().target(isCompensate ? transferCancelUri : transferConfirmUri);
-        webTarget.request().header(TRANSFER_ID, lraId)
-                .post(Entity.text("")).readEntity(String.class);
-
-        // return status
-        return Response.ok("transfer status:" + returnString).build();
+        log.info("Started new LRA : " + lraId);
+        withdraw(fromAccount, amount);
+        deposit(toAccount, amount);
+        return Response.ok("transfer successful").build();
 
     }
-
-    private String withdraw(long accountId, long amount) {
-        log.info("withdraw accountId = " + accountId + ", amount = " + amount);
-        WebTarget webTarget = ClientBuilder.newClient().target(withdrawUri).path("/")
-                .queryParam("accountId", accountId)
-                .queryParam("amount", amount);
-        URI lraId = Current.peek();
-        log.info("withdraw lraId = " + lraId);
-        String withdrawOutcome = webTarget.request().header(LRA_HTTP_CONTEXT_HEADER, lraId)
-                .post(Entity.text("")).readEntity(String.class);
+    private String withdraw(String accountName, long depositAmount) {
+        log.info("withdraw accountName = " + accountName + ", depositAmount = " + depositAmount);
+        WebTarget webTarget =
+                ClientBuilder.newClient().target(withdrawUri).path("/")
+                        .queryParam("accountName", accountName)
+                        .queryParam("withdrawAmount", depositAmount);
+        String withdrawOutcome = webTarget.request().post(Entity.text("")).getEntity().toString();
         return withdrawOutcome;
     }
-
-    private String deposit(long accountId, long amount) {
-        log.info("deposit accountId = " + accountId + ", amount = " + amount);
-        WebTarget webTarget = ClientBuilder.newClient().target(depositUri).path("/")
-                .queryParam("accountId", accountId)
-                .queryParam("amount", amount);
-        URI lraId = Current.peek();
-        log.info("deposit lraId = " + lraId);
-        String depositOutcome = webTarget.request().header(LRA_HTTP_CONTEXT_HEADER, lraId)
-                .post(Entity.text("")).readEntity(String.class);
-        ;
-        return depositOutcome;
-    }
-
-    @POST
-    @Path("/processconfirm")
-    @Produces(MediaType.APPLICATION_JSON)
-    @LRA(value = LRA.Type.MANDATORY)
-    public Response processconfirm(@HeaderParam(LRA_HTTP_CONTEXT_HEADER) String lraId) throws NotFoundException {
-        log.info("Process confirm for transfer : " + lraId);
-        return Response.ok().build();
-    }
-
-    @POST
-    @Path("/processcancel")
-    @Produces(MediaType.APPLICATION_JSON)
-    @LRA(value = LRA.Type.MANDATORY, cancelOn = Response.Status.OK)
-    public Response processcancel(@HeaderParam(LRA_HTTP_CONTEXT_HEADER) String lraId) throws NotFoundException {
-        log.info("Process cancel for transfer : " + lraId);
-        return Response.ok().build();
-    }
-
-    @POST
-    @Path("/confirm")
-    @Produces(MediaType.APPLICATION_JSON)
-    @LRA(value = LRA.Type.NOT_SUPPORTED)
-    public Response confirm(@HeaderParam(TRANSFER_ID) String transferId) throws NotFoundException {
-        log.info("Received confirm for transfer : " + transferId);
-        String confirmOutcome = ClientBuilder.newClient().target(transferProcessConfirmUri).request()
-                .header(LRA_HTTP_CONTEXT_HEADER, transferId)
-                .post(Entity.text("")).readEntity(String.class);
-        return Response.ok(confirmOutcome).build();
-    }
-
-    @POST
-    @Path("/cancel")
-    @Produces(MediaType.APPLICATION_JSON)
-    @LRA(value = LRA.Type.NOT_SUPPORTED, cancelOn = Response.Status.OK)
-    public Response cancel(@HeaderParam(TRANSFER_ID) String transferId) throws NotFoundException {
-        log.info("Received cancel for transfer : " + transferId);
-        String confirmOutcome = ClientBuilder.newClient().target(transferProcessCancelUri).request()
-                .header(LRA_HTTP_CONTEXT_HEADER, transferId)
-                .post(Entity.text("")).readEntity(String.class);
-        return Response.ok(confirmOutcome).build();
+    private String deposit(String accountName, long depositAmount) {
+        log.info("deposit accountName = " + accountName + ", depositAmount = " + depositAmount);
+        WebTarget webTarget =
+                ClientBuilder.newClient().target(depositUri).path("/")
+                        .queryParam("accountName", accountName)
+                        .queryParam("depositAmount", depositAmount);
+        String withdrawOutcome = webTarget.request().post(Entity.text("")).getEntity().toString();
+        return withdrawOutcome;
     }
 
 }
