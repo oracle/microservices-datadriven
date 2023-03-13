32eea83213f3d6ba91df81bd749e528ad118182d
Andy Tael <andy.tael@yahoo.com>
2023-03-13 14:04:30 -0500

    Bugfixes

--- workshops/dcms-ords-sec/output.tf
+++ workshops/dcms-ords-sec/output.tf
@@ -2,7 +2,7 @@
 # All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 output "lb_address" {
-  value       = oci_load_balancer.lb.ip_addresses[0]
+  value       = oci_load_balancer.lb.ip_address_details[0].ip_address
   description = "The Pubic facing IP Address assigned to the Load Balancer"
 }
 
