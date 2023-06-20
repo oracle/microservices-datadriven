b86b7764dacec1bca68bb72077316a5f4955c4d3
Andy Tael <andy.tael@yahoo.com>
2023-06-20 12:27:53 -0500

    Initial Commit
    Initial Commit

---
+++ cloudbank-v3/spring-apps/account/src/main/java/com/example/accounts/repository/JournalRepository.java
@@ -0,0 +1,15 @@
+// Copyright (c) 2023, Oracle and/or its affiliates. 
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/ 
+
+package com.example.accounts.repository;
+
+import com.example.accounts.model.Journal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface JournalRepository extends JpaRepository<Journal, Long> {
+    Journal findJournalByLraIdAndJournalType(String lraId, String journalType);
+    List<Journal> findByAccountId(long accountId);
+    List<Journal> findJournalByAccountId(long accountId);
+}
---
+++ cloudbank-v3/spring-apps/customer/src/main/java/com/example/customer/CustomerApplication.java
@@ -0,0 +1,17 @@
+// Copyright (c) 2023, Oracle and/or its affiliates. 
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/ 
+package com.example.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
+
+}
