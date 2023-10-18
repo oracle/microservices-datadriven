cf798fe2d12266c7a2d4aa7c8a20f51dd374ab15
Andy Tael <andy.tael@yahoo.com>
2023-10-17 19:11:10 -0500

    Checkstyle stuff

--- cloudbank-v3/spring-apps-spring3/creditscore/src/main/java/com/example/creditscore/controller/CreditScoreController.java
+++ cloudbank-v3/spring-apps-spring3/creditscore/src/main/java/com/example/creditscore/controller/CreditScoreController.java
@@ -1,23 +1,26 @@
-// Copyright (c) 2023, Oracle and/or its affiliates. 
+// Copyright (c) 2023, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/ 
 
 package com.example.creditscore.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import lombok.extern.slf4j.Slf4j;
-
 import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 @Slf4j
 public class CreditScoreController {
 
+    /**
+     * Get a random creditscore at current date.
+     * @return Returns creditscore at current date
+     */
     @GetMapping("/creditscore")
     public Map<String, String> getCreditScore() {
         log.info("CREDITSCORE: getCreditScore");
