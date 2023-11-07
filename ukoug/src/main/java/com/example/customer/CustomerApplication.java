545c9cd149ff44073ea5e46ab74eb8711907c323
Andy Tael <andy.tael@yahoo.com>
2023-11-07 11:17:35 -0600

    Eureka stuff

--- ukoug/src/main/java/com/example/customer/CustomerApplication.java
+++ ukoug/src/main/java/com/example/customer/CustomerApplication.java
@@ -5,10 +5,10 @@ package com.example.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-// @EnableDiscoveryClient
+@EnableDiscoveryClient
 public class CustomerApplication {
 
     public static void main(String[] args) {
