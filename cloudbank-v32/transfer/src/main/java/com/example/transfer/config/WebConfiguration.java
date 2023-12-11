84089e8de028a7f930159ff875cdf86785b41a9c
Andy Tael <andy.tael@yahoo.com>
2023-12-11 14:03:15 -0600

    springboot 3.2 initial commit

---
+++ cloudbank-v32/transfer/src/main/java/com/example/transfer/config/WebConfiguration.java
@@ -0,0 +1,18 @@
+// Copyright (c) 2023, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+package com.example.transfer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SuppressWarnings("deprecation") // TODO: Needs to be removed
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseTrailingSlashMatch(true);
+    }
+}
