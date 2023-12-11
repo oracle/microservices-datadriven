ac453c6a72589ed6c6e80e875ef6598dedddb829
Andy Tael <andy.tael@yahoo.com>
2023-12-11 14:03:32 -0600

    spingboot 3.2 initial commit

---
+++ cloudbank-v32/checks/src/main/java/com/example/checks/ChecksApplication.java
@@ -0,0 +1,74 @@
+// Copyright (c) 2023, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+package com.example.checks;
+
+import jakarta.jms.ConnectionFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+// import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableJms
+// @EnableEurekaClient
+@EnableDiscoveryClient
+public class ChecksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChecksApplication.class, args);
+    }
+
+    /**
+     * Serialize message content to json using TextMessage.
+     * @return TO-DO
+     */
+    @Bean 
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+    /**
+     * TO-DO.
+     * @param connectionFactory TO-DO
+     * @return TO-DO
+     */
+    @Bean
+    public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
+        JmsTemplate jmsTemplate = new JmsTemplate();
+        jmsTemplate.setConnectionFactory(connectionFactory);
+        jmsTemplate.setMessageConverter(jacksonJmsMessageConverter());
+        return jmsTemplate;
+    }
+
+    /**
+     * TO-DO.
+     * @param connectionFactory TO-DO
+     * @param configurer TO-DO
+     * @return TO-DO
+     */
+    @Bean
+    public JmsListenerContainerFactory<?> factory(ConnectionFactory connectionFactory,
+                                                  DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        // This provides all boot's default to this factory, including the message converter
+        configurer.configure(factory, connectionFactory);
+        // You could still override some of Boot's default if necessary.
+        return factory;
+    }
+
+}
