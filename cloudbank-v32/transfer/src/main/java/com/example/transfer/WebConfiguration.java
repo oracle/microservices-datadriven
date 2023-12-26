8289ff347fc01bbadb2f0aa620712542b06b8476
Andy Tael <andy.tael@yahoo.com>
2023-12-25 18:20:40 -0600

    Banner updates, pom cleanup and fixes

--- cloudbank-v32/transfer/src/main/java/com/example/transfer/WebConfiguration.java
+++ cloudbank-v32/transfer/src/main/java/com/example/transfer/WebConfiguration.java
@@ -3,16 +3,16 @@
 
 package com.example.transfer;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@SuppressWarnings("deprecation") // TODO: Needs to be removed
-@Configuration
-public class WebConfiguration implements WebMvcConfigurer {
-
-    @Override
-    public void configurePathMatch(PathMatchConfigurer configurer) {
-        configurer.setUseTrailingSlashMatch(true);
-    }
-}
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@SuppressWarnings("deprecation") // TODO: Needs to be removed
+//@Configuration
+//public class WebConfiguration implements WebMvcConfigurer {
+//
+//    @Override
+//    public void configurePathMatch(PathMatchConfigurer configurer) {
+//        configurer.setUseTrailingSlashMatch(true);
+//    }
+//}
