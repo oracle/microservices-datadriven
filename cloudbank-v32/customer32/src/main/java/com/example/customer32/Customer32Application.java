28e8a1eb6af0bac876bc7e754939bf83214b8198
Andy Tael <andy.tael@yahoo.com>
2024-01-11 13:11:37 -0600

    Fixes for ObaaS deployment

--- cloudbank-v32/customer32/src/main/java/com/example/customer32/Customer32Application.java
+++ cloudbank-v32/customer32/src/main/java/com/example/customer32/Customer32Application.java
@@ -5,8 +5,10 @@ package com.example.customer32;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Customer32Application {
 
     public static void main(String[] args) {
