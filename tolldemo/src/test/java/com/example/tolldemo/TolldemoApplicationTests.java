c78b5a53c4160d05756546e5c39b33342827643c
Andy Tael <andy.tael@yahoo.com>
2024-04-16 14:48:18 -0500

    Directory and dummy application

---
+++ tolldemo/src/test/java/com/example/tolldemo/TolldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tolldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TolldemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
