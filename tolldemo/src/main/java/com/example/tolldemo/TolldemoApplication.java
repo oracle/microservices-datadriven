c359f6012831c251bf1932742a64172e11529cb6
Andy Tael <andy.tael@yahoo.com>
2024-04-16 15:02:57 -0500

    Directory and dummy application (#872)
    * Directory and dummy application
    
    * Delete directory

---
+++ tolldemo/src/main/java/com/example/tolldemo/TolldemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tolldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TolldemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TolldemoApplication.class, args);
+    }
+
+}
