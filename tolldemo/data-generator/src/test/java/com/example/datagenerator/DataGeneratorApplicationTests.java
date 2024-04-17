235d70047f1f489d9b3ef6167646ec0238b9710e
Mark Nelson <mark.x.nelson@oracle.com>
2024-04-17 15:57:15 -0400

    collecting inputs for data-gen
    Signed-off-by: Mark Nelson <mark.x.nelson@oracle.com>

---
+++ tolldemo/data-generator/src/test/java/com/example/datagenerator/DataGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datagenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
