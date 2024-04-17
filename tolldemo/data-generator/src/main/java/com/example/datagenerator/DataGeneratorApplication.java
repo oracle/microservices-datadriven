d59196a5d35435195b6f71de598337ae79fadd55
Mark Nelson <mark.x.nelson@oracle.com>
2024-04-17 16:05:48 -0400

    WIP: tollbooth demo data generator (#874)
    * collecting inputs for data-gen
    
    ---------
    
    Signed-off-by: Mark Nelson <mark.x.nelson@oracle.com>

---
+++ tolldemo/data-generator/src/main/java/com/example/datagenerator/DataGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.datagenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataGeneratorApplication.class, args);
+	}
+
+}
