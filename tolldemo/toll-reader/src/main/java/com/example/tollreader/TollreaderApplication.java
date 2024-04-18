41c2104249fd1a3a294558d2ccf1c3f0252d194f
Andy Tael <andy.tael@yahoo.com>
2024-04-18 15:30:27 -0500

    Updates

--- tolldemo/toll-reader/src/main/java/com/example/tollreader/TollreaderApplication.java
+++ tolldemo/toll-reader/src/main/java/com/example/tollreader/TollreaderApplication.java
@@ -1,8 +1,10 @@
 package com.example.tollreader;
 
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import jakarta.jms.Session;
 import jakarta.json.Json;
 import jakarta.json.JsonObject;
-import jakarta.jms.ConnectionFactory;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 import java.security.SecureRandom;
@@ -11,19 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
-
-// dbms_aqadm.create_transactional_event_queue (queue_name => 'TollGate', multiple_consumers => true);
-// dbms_aqadm.set_queue_parameter('TollGate', 'KEY_BASED_ENQUEUE', 2);
-// dbms_aqadm.set_queue_parameter('TollGate', 'SHARD_NUM', 5);
-// dbms_aqadm.start_queue('TollGate');
-// end;
-// /
+import org.springframework.jms.core.MessageCreator;
 
 @EnableJms
 @SpringBootApplication
@@ -34,43 +26,40 @@ public class TollreaderApplication implements CommandLineRunner {
 	private static final Integer maxNumber = 99999;
 
 	@Autowired
-    private JmsTemplate jmsTemplate;
-	
+	private JmsTemplate jmsTemplate;
+
 	private static <T extends Enum<?>> T randomEnum(Class<T> clazz) {
 		int x = random.nextInt(clazz.getEnumConstants().length);
 		return clazz.getEnumConstants()[x];
 	}
 
 	private void sendMessage(JsonObject tolldata) {
-        jmsTemplate.convertAndSend("TollGate", tolldata);
-    }
+		// jmsTemplate.convertAndSend("TollGate", tolldata);
+		jmsTemplate.send("TollGate", new MessageCreator() {
 
-	// Can I move this to a different class? service, component?
-	// @Bean
-    // public MessageConverter jacksonJmsMessageConverter() {
-    //     MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-    //     converter.setTargetType(MessageType.TEXT);
-    //     converter.setTypeIdPropertyName("_type");
-    //     return converter;
-    // }
+			@SuppressWarnings("null")
+			@Override
+			public Message createMessage(Session session) throws JMSException {
+				return session.createTextMessage(tolldata.toString());
+			}
 
-	// // Can I move this to a different class? service, component?
-    // @Bean
-    // public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
-    //     JmsTemplate jmsTemplate = new JmsTemplate();
-    //     jmsTemplate.setConnectionFactory(connectionFactory);
-    //     jmsTemplate.setMessageConverter(jacksonJmsMessageConverter());
-    //     return jmsTemplate;
-    // }
+		});
+	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		// System.out.println("Args: " + args[0] + " " + args.length);
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		LocalDateTime now = LocalDateTime.now();
 		String dateTimeString = now.format(formatter);
+		Integer sleepTime = 1000;
+		Integer numRecords = 100000;
+		if (args.length > 0 && !args[0].isBlank()) {
+			sleepTime = Integer.parseInt(args[0]);
+		}
 
-		for (int i = 0; i < 10; i++) {
-			Thread.sleep(1000);
+		for (int i = 0; i < numRecords; i++) {
+			Thread.sleep(sleepTime);
 
 			Integer licNumber = random.nextInt(maxNumber - minNumber) + minNumber;
 			Integer tagId = random.nextInt(maxNumber - minNumber) + minNumber;
@@ -79,12 +68,12 @@ public class TollreaderApplication implements CommandLineRunner {
 			String carType = randomEnum(CarType.class).toString();
 
 			JsonObject data = Json.createObjectBuilder()
-			.add("accountnumber", accountNumber) // This could be looked up in the DB from the tagId?
-			.add("license-plate", state + "-" + licNumber.toString()) // This could be looked up in the DB from the tagId?
-			.add("cartype", carType) // This could be looked up in the DB from the tagId?
-			.add("tagid", tagId)
-			.add("timestamp", dateTimeString)
-			.build();
+				.add("accountnumber", accountNumber) // This could be looked up in the DB from the tagId?
+				.add("license-plate", state + "-" + licNumber.toString()) // This could be looked up in the DB from the tagId?
+				.add("cartype", carType) // This could be looked up in the DB from the tagId?
+				.add("tagid", tagId)
+				.add("timestamp", dateTimeString)
+				.build();
 
 			System.out.println(data);
 			sendMessage(data);
