234e27a3f452f2269ad84362cc4c6cf98bc1f666
Andy Tael <andy.tael@yahoo.com>
2024-04-30 15:30:52 -0500

    sql updates

--- tolldemo/journal-app/src/main/resources/schema.sql
+++ tolldemo/journal-app/src/main/resources/schema.sql
@@ -6,10 +6,11 @@ create table if not exists journal (
     tag_id varchar2(64),
     license_plate varchar2(10),
     vehicle_type varchar2(10),
-    toll_date varchar2(25)
+    toll_date varchar2(25),
+    primary_key(journal_id)
 );
 
-ALTER TABLE journal ADD CONSTRAINT journal_PK PRIMARY KEY (journal_id) USING INDEX LOGGING;
+-- ALTER TABLE journal ADD CONSTRAINT journal_PK PRIMARY KEY (journal_id) USING INDEX LOGGING;
 
 drop table if exists customer cascade constraints;
 
@@ -20,10 +21,11 @@ create table if not exists customer (
     last_name varchar2(256),
     address varchar2(256),
     city varchar2(256),
-    zipcode varchar2(256)
+    zipcode varchar2(256),
+    primary key(customer_id)
 );
 
-alter table customer add constraint customer_pk primary key(customer_id) using index logging;
+-- alter table customer add constraint customer_pk primary key(customer_id) using index logging;
 
 drop table if exists vehicle cascade constraints;
 
@@ -34,7 +36,11 @@ create table if not exists vehicle (
     state varchar2(256),
     license_plate varchar2(256),
     vehicle_type varchar2(256),
-    image varchar2(256)
+    image varchar2(256),
+    primary key(vehicle_id),
+    foreign key(customer_id) references customer(customer_id)
 );
 
-alter table vehicle add constraint vehicle_pk primary key(vehicle_id) using index logging;
+-- alter table vehicle add constraint vehicle_pk primary key(vehicle_id) using index logging;
+
+-- alter table "TOLLDEMO"."VEHICLE" add constraint customer_fk foreign key("CUSTOMER_ID") references "CUSTOMER"("CUSTOMER_ID")
