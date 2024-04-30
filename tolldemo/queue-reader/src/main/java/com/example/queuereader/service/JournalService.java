f25481e173b4e39ad153839016abf6a5e878e980
Andy Tael <andy.tael@yahoo.com>
2024-04-30 11:59:50 -0500

    Feign work

--- tolldemo/queue-reader/src/main/java/com/example/queuereader/service/JournalService.java
+++ tolldemo/queue-reader/src/main/java/com/example/queuereader/service/JournalService.java
@@ -4,15 +4,18 @@ import com.example.queuereader.client.JournalClient;
 //import lombok.RequiredArgsConstructor;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class JournalService {
 
     private final JournalClient journalClient;
 
     public void journal(JsonNode tollData) {
+        log.info("Journal data: {}", tollData);
         journalClient.journal(tollData);
     }
 }
