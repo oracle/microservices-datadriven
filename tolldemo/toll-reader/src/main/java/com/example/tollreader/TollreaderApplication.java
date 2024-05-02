a57a284a56a8e8378ffaaf531aabfd02aefd4982
Andy Tael <andy.tael@yahoo.com>
2024-05-02 09:22:29 -0500

    obaas

--- tolldemo/toll-reader/src/main/java/com/example/tollreader/TollreaderApplication.java
+++ tolldemo/toll-reader/src/main/java/com/example/tollreader/TollreaderApplication.java
@@ -5,6 +5,7 @@ package com.example.tollreader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.jms.annotation.EnableJms;
 import com.example.tollreader.data.DataBean;
