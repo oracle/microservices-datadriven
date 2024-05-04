2e58f53b4c6ef01b1ab0be3ddb8be51bbdf6e516
Andy Tael <andy.tael@yahoo.com>
2024-05-04 18:22:58 -0500

    OBaaS and OTEL configuration for tolldemo (#886)
    OTEL

--- tolldemo/toll-reader/src/main/java/com/example/tollreader/TollreaderApplication.java
+++ tolldemo/toll-reader/src/main/java/com/example/tollreader/TollreaderApplication.java
@@ -5,6 +5,7 @@ package com.example.tollreader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.jms.annotation.EnableJms;
 import com.example.tollreader.data.DataBean;
@@ -13,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 @EnableJms
 @SpringBootApplication
 @Slf4j
+@EnableDiscoveryClient
 public class TollreaderApplication {
 
   private static ConfigurableApplicationContext context;
