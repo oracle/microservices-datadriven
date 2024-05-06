e57448a32085c569955a9bca32e9c464f20e5c3d
paulparkinson <paul.parkinson@gmail.com>
2024-05-06 16:27:37 -0400

    switch to arm for graalvm due to cloudshell change in default

--- grabdish/utils/java-builds.sh
+++ grabdish/utils/java-builds.sh
@@ -20,7 +20,7 @@ done
 # Install Graal
 while ! state_done GRAAL; do
   if ! test -d ~/graalvm-ce-java11-20.1.0; then
-    curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.1.0/graalvm-ce-java11-linux-amd64-20.1.0.tar.gz | tar xz
+    curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.2.0/graalvm-ce-java11-linux-aarch64-20.2.0.tar.gz | tar xz
     mv graalvm-ce-java11-20.1.0 ~/
   fi
   state_set_done GRAAL
