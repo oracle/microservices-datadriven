8a670a06a020128a8bd9c1e4b9c93be489cddca5
Andy Tael <andy.tael@yahoo.com>
2024-05-10 12:02:55 -0500

    WIP

--- workshops/txeventq-kafka/cloud-setup/java/graalvm-install.sh
+++ workshops/txeventq-kafka/cloud-setup/java/graalvm-install.sh
@@ -5,33 +5,34 @@
 # Fail on error
 set -eu
 
-GRAALVM_VERSION=${1:-"22.2.0"}
+GRAALVM_VERSION=${1:-"17.0.9"}
 OS_NAME=$(uname)
 
 # Install GraalVM
-# https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.2.0/graalvm-ce-java11-linux-amd64-22.2.0.tar.gz
-if ! test -d ~/graalvm-ce-java11-"${GRAALVM_VERSION}"; then
+# https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-17.0.9/graalvm-community-jdk-17.0.9_linux-aarch64_bin.tar.gz
+if ! test -d ~/graalvm-community-jdk-"${GRAALVM_VERSION}"; then
   echo "$(date): Installing graalvm-ce-java11-${GRAALVM_VERSION}"
-  (cd ~ && curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-"${GRAALVM_VERSION}"/graalvm-ce-java11-"${OS_NAME}"-amd64-"${GRAALVM_VERSION}".tar.gz | tar xz)
+  (cd ~ && curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-"${GRAALVM_VERSION}"/graalvm-community-jdk-17.0.9_"${OS_NAME}"-aarch64-"${GRAALVM_VERSION}".tar.gz | tar xz)
 #  mv graalvm-ce-java11-${GRAALVM_VERSION} ~/
 fi
 
 if ! [[ $OS_NAME == *"darwin"* ]]; then
   # Assume linux
-  ~/graalvm-ce-java11-"${GRAALVM_VERSION}"/bin/gu install native-image
+  ~/graalvm-community-jdk-"${GRAALVM_VERSION}+9.1"/bin/gu install native-image
   export JAVA_HOME=~/graalvm-ce-java11-${GRAALVM_VERSION}
-else
-  # We are on Mac doing local dev
-  ~/graalvm-ce-java11-"${GRAALVM_VERSION}"/Contents/Home/bin/gu install native-image
-  export JAVA_HOME=~/graalvm-ce-java11-${GRAALVM_VERSION}/Contents/Home;
-  echo "$(date): JAVA_HOME ${JAVA_HOME}"
 fi
+# else
+#   # We are on Mac doing local dev
+#   ~/graalvm-ce-java11-"${GRAALVM_VERSION}"/Contents/Home/bin/gu install native-image
+#   export JAVA_HOME=~/graalvm-ce-java11-${GRAALVM_VERSION}/Contents/Home;
+#   echo "$(date): JAVA_HOME ${JAVA_HOME}"
+# fi
 
 export PATH=$JAVA_HOME/bin/:$PATH
 echo "$(date): PATH ${PATH}"
 
 if ! state_done CONTAINER_ENG_SETUP; then
-  echo "$(date): Installing GraalVM CE Java 11 Image"
+  echo "$(date): GraalVM for JDK 17 Community 17.0.9"
   docker pull ghcr.io/graalvm/graalvm-ce:ol8-java11 --quiet
   state_set CONTAINER_ENG_SETUP "ghcr.io/graalvm/graalvm-ce:ol8-java11"
   echo
