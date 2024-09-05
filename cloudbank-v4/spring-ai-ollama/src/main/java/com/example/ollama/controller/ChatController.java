04d10e1756817bbd561451ee4d3b493fd2bc4faa
Mark Nelson <mark.x.nelson@oracle.com>
2024-09-05 16:31:34 -0400

    WIP
    Signed-off-by: Mark Nelson <mark.x.nelson@oracle.com>

---
+++ cloudbank-v4/spring-ai-ollama/src/main/java/com/example/ollama/controller/ChatController.java
@@ -0,0 +1,43 @@
+// Copyright (c) 2024, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+package com.example.ollama.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.api.OllamaModel;
+import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+
+    final ChatModel chatModel;
+
+    public ChatController(ChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @PostMapping
+    public String chat(@RequestBody String question) {
+        
+        ChatResponse response = chatModel.call(
+            new Prompt(question,
+            OllamaOptions.builder()
+            .withModel(OllamaModel.LLAMA3)
+            .withTemperature(0.4f)
+            .build()
+        ));
+
+        return response.toString();
+        
+    }
+    
+
+}
