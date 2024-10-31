4cc157b4b8d4c88efdf7054b61b69b593d38373f
Andy Tael <andy.tael@yahoo.com>
2024-10-31 11:14:25 -0500

    copyright changes

--- code-teq/javaTeq/user_perm.sql
+++ code-teq/javaTeq/user_perm.sql
@@ -1,3 +1,6 @@
+-- Copyright (c) 2022, 2024, Oracle and/or its affiliates.
+-- Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 -- This must be executed as SYS
 create user testuser identified by Welcome12345;
 grant resource, connect, unlimited tablespace to testuser;
