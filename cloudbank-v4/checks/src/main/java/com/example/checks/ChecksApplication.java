2db578114c6f2eb102a7ceac603c400d79d744d4
Andy Tael <andy.tael@yahoo.com>
2024-12-03 15:57:25 -0600

    Version upgrades and fixes (#997)

--- cloudbank-v4/checks/src/main/java/com/example/checks/ChecksApplication.java
+++ cloudbank-v4/checks/src/main/java/com/example/checks/ChecksApplication.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2023, Oracle and/or its affiliates.
+// Copyright (c) 2023, 2024, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
 package com.example.checks;
@@ -8,7 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-// import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
@@ -31,9 +30,10 @@ public class ChecksApplication {
 
     /**
      * Serialize message content to json using TextMessage.
+     * 
      * @return TO-DO
      */
-    @Bean 
+    @Bean
     public MessageConverter jacksonJmsMessageConverter() {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
@@ -43,6 +43,7 @@ public class ChecksApplication {
 
     /**
      * TO-DO.
+     * 
      * @param connectionFactory TO-DO
      * @return TO-DO
      */
@@ -56,15 +57,17 @@ public class ChecksApplication {
 
     /**
      * TO-DO.
+     * 
      * @param connectionFactory TO-DO
-     * @param configurer TO-DO
+     * @param configurer        TO-DO
      * @return TO-DO
      */
     @Bean
     public JmsListenerContainerFactory<?> factory(ConnectionFactory connectionFactory,
-                                                  DefaultJmsListenerContainerFactoryConfigurer configurer) {
+            DefaultJmsListenerContainerFactoryConfigurer configurer) {
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        // This provides all boot's default to this factory, including the message converter
+        // This provides all boot's default to this factory, including the message
+        // converter
         configurer.configure(factory, connectionFactory);
         // You could still override some of Boot's default if necessary.
         return factory;
