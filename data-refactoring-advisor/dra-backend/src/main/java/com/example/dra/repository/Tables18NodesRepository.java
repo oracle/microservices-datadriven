bbd99d2dfef53ad51646c538f399209f95a33685
Doug Drechsel <doug.drechsel@oracle.com>
2025-06-12 13:45:47 -0400

    Remove Data Refactoring Advisor (#1068)

--- data-refactoring-advisor/dra-backend/src/main/java/com/example/dra/repository/Tables18NodesRepository.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2023, Oracle and/or its affiliates.
-// Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl/ 
-
-package com.example.dra.repository;
-
-import com.example.dra.entity.Tables18NodesEntity;
-
-public interface Tables18NodesRepository extends BaseJpaRepository<Tables18NodesEntity, Long> {
-}
