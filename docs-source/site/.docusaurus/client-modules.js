2d82968dc79916e73779af2f0917402adc96a463
Andy Tael <andy.tael@yahoo.com>
2025-10-07 14:28:43 -0500

    Documentation update
    Signed-off-by: Andy Tael <andy.tael@yahoo.com>

---
+++ docs-source/site/.docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/Users/atael/Documents/GitHub/microservices-datadriven/docs-source/site/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/atael/Documents/GitHub/microservices-datadriven/docs-source/site/node_modules/@docusaurus/plugin-google-gtag/lib/gtag"),
+  require("/Users/atael/Documents/GitHub/microservices-datadriven/docs-source/site/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/atael/Documents/GitHub/microservices-datadriven/docs-source/site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/atael/Documents/GitHub/microservices-datadriven/docs-source/site/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/atael/Documents/GitHub/microservices-datadriven/docs-source/site/src/css/custom.css"),
+];
