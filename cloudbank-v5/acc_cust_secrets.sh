c660b08da74b9e41695a6a460640df4491004dcb
Andy Tael <andy.tael@yahoo.com>
2025-11-18 11:09:42 -0600

    Cloudbank v5 updates
    Signed-off-by: Andy Tael <andy.tael@yahoo.com>

--- cloudbank-v5/acc_cust_secrets.sh
+++ cloudbank-v5/acc_cust_secrets.sh
@@ -4,16 +4,16 @@
 # Adjust the namespace and secret values as needed
 
 kubectl -n obaas-dev create secret generic account-db-secrets \
-  --from-literal=db.name=helmdb \
+  --from-literal=db.name=helmtest \
   --from-literal=db.username=account \
-  --from-literal=db.password=YOUR_PASSWORD \
-  --from-literal=db.service=helmdb_tp \
+  --from-literal=db.password=Welcome-12345 \
+  --from-literal=db.service=helmtest_tp \
   --from-literal=db.lb_username=admin \
-  --from-literal=db.lb_password=YOUR_PASSWORD
+  --from-literal=db.lb_password=Welcome-12345
 kubectl -n obaas-dev create secret generic customer-db-secrets \
-  --from-literal=db.name=helmdb \
+  --from-literal=db.name=helmtest \
   --from-literal=db.username=customer \
-  --from-literal=db.password=YOUR_PASSWORD \
-  --from-literal=db.service=helmdb_tp \
+  --from-literal=db.password=Welcome-12345 \
+  --from-literal=db.service=helmtest_tp \
   --from-literal=db.lb_username=admin \
-  --from-literal=db.lb_password=YOUR_PASSWORD
+  --from-literal=db.lb_password=Welcome-12345
