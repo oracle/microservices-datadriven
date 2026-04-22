e9e484b6f07b9f83a095ebe0ec2c52a8a496d31e
Andy Tael <andy.tael@yahoo.com>
2026-04-22 13:30:11 -0500

    Remove Cloudbank v4 & v32
    Signed-off-by: Andy Tael <andy.tael@yahoo.com>

--- cloudbank-v4/local-environment/txeventq-privs.sql
+++
@@ -1,6 +0,0 @@
-alter session set container=freepdb1;
-grant execute on dbms_aq to system with grant option;
-grant execute on dbms_aqadm to system with grant option;
-grant execute on dbms_aqin to system with grant option;
-grant execute on dbms_aqjms TO system with grant option;
-grant execute on dbms_aqjms_internal to system with grant option;
